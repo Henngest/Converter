e6ee4640e8e4479c1edc5676f291d9187b6a0cea
Filip Danilovski <filip_danilovski@hotmail.com>
2021-08-25 19:32:21 +0200

    Update constructor and get functions in ConverterClass

--- ConverterClass.cs
+++ ConverterClass.cs
@@ -8,20 +8,30 @@ namespace Converter
 {
     class ConverterClass
     {
+        private Dictionary<String, List<String>> unitsDictionary;
+        public ConverterClass()
+        {
+            unitsDictionary = new Dictionary<string, List<string>>();
+
+            List<String> lengthUnits = new List<String>();
+            lengthUnits.Add("cm");
+            lengthUnits.Add("m");
+            lengthUnits.Add("km");
+            unitsDictionary.Add("Length", lengthUnits);
+
+            List<String> weightUnits = new List<String>();
+            weightUnits.Add("g");
+            weightUnits.Add("kg");
+            weightUnits.Add("t");
+            unitsDictionary.Add("Weight", weightUnits);
+        }
         public List<String> getUnitTypes() 
         {
-            List<String> lista = new List<String>();
-            lista.Add("Length");
-            lista.Add("Weight");
-            return lista;
+            return unitsDictionary.Keys.ToList();
         }
         public List<String> getUnits(String unitType) 
         {
-            List<String> lista = new List<String>();
-            lista.Add("cm");
-            lista.Add("m");
-            lista.Add("km");
-            return lista;
+            return unitsDictionary[unitType];
         }
         public double convert(String fromUnit, String toUnit, double value) 
         {
