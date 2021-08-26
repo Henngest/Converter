0c82d0b158506996c59aeb5d962329741ae9576b
Filip Danilovski <filip_danilovski@hotmail.com>
2021-08-26 20:56:49 +0200

    Extend list of length and weight units

--- ConverterClass.cs
+++ ConverterClass.cs
@@ -15,20 +15,40 @@ namespace Converter
             unitsDictionary = new Dictionary<string, List<Unit>>();
             
             List<Unit> lengthUnits = new List<Unit>();
+            unitObject = new Unit("nm", 0.000000001);
+            lengthUnits.Add(unitObject);
+            unitObject = new Unit("μm", 0.000001);
+            lengthUnits.Add(unitObject);
+            unitObject = new Unit("mm", 0.001);
+            lengthUnits.Add(unitObject);
             unitObject = new Unit("cm", 0.01);
             lengthUnits.Add(unitObject);
             unitObject = new Unit("m", 1);
             lengthUnits.Add(unitObject);
             unitObject = new Unit("km", 1000);
             lengthUnits.Add(unitObject);
+            unitObject = new Unit("in", 0.0254);
+            lengthUnits.Add(unitObject);
+            unitObject = new Unit("ft", 0.3048);
+            lengthUnits.Add(unitObject);
+            unitObject = new Unit("yd", 0.9144);
+            lengthUnits.Add(unitObject);
+            unitObject = new Unit("mi", 1609.34);
+            lengthUnits.Add(unitObject);
             unitsDictionary.Add("Length", lengthUnits);
 
             List<Unit> weightUnits = new List<Unit>();
+            unitObject = new Unit("mg", 0.000001);
+            weightUnits.Add(unitObject);
             unitObject = new Unit("g", 0.001);
             weightUnits.Add(unitObject);
             unitObject = new Unit("kg", 1);
             weightUnits.Add(unitObject);
             unitObject = new Unit("t", 1000);
+            weightUnits.Add(unitObject);
+            unitObject = new Unit("lbs", 0.453592);
+            weightUnits.Add(unitObject);
+            unitObject = new Unit("oz", 0.0283495);
             weightUnits.Add(unitObject);
             unitsDictionary.Add("Weight", weightUnits);
         }
